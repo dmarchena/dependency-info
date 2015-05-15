8f5ffdbc729c8965ee50aaa2a8b2a0a0a2bc2177
dmarchena <dvmarchena@gmail.com>
2015-05-15 18:31:36 +0000

    Mocha tests ready

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
+var mocha = require('gulp-mocha');
 
 gulp.task('lint', function () {
   return gulp
-  	.src(['./*.js', './test/*.js'])
+  	.src(['./*.js', './test/**/*.js'])
   	.pipe(jshint())
   	.pipe(jshint.reporter(stylish))
   	.pipe(jshint.reporter('fail'));
 });
 
-gulp.task('default', ['lint']);
+gulp.task('test', ['lint'], function () {
+  return gulp
+  	.src('./test/**/*.js', { read: false })
+    .pipe(mocha({
+      reporter: 'spec',
+      globals: {
+        should: require('should')
+      }
+    }));
+});
+
+gulp.task('default', ['test']);
