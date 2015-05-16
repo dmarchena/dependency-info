35ce9c59aba4015ac3f169b1186fa1bd887fc76b
David Marchena <dvmarchena@gmail.com>
2015-05-16 23:42:12 +0200

    Add test 'should only retrieve production dependencies'

--- test/scenarios/npm/test.js
+++ test/scenarios/npm/test.js
@@ -7,11 +7,22 @@ var path = require('path'),
     testDataDir = path.join(scenarioDir, '/data/'),
     Dependency = require(path.join(rootDir, '/lib/dependency.js')),
     dependenciesInfo = require(path.join(rootDir, '/lib/'));
+
+function isDependencyInTree(dependencyObj, tree) {
+  for (var i=0, length = tree.length; i<length; i++) {
+    var p = tree[i];
+    if (dependencyObj.isEqualTo(p)) {
+      return true;
+    }
+  }
+  return false;
+}
     
 describe('NPM Dependency search:', function () {
 
-  var dependencyWillFind = new Dependency('module-1', path.join(testDataDir, '/node_modules/module-1/')),
-      dependencyWontFind = new Dependency('module-3', path.join(testDataDir, '/node_modules/module-3/')),
+  var module1 = new Dependency('module-1', path.join(testDataDir, '/node_modules/module-1/')),
+      module2 = new Dependency('module-2', path.join(testDataDir, '/node_modules/module-2/')),
+      module3 = new Dependency('module-3', path.join(testDataDir, '/node_modules/module-3/')),
       searchRootPath = testDataDir,
       dependencies = dependenciesInfo({ path: searchRootPath });
 
@@ -26,19 +37,20 @@ describe('NPM Dependency search:', function () {
   }
 
   it('should filter dependencies by its keywords', function () {
-    var arrayContainsDependencyWithKeyword = false,
-        arrayContainsDependencyWithoutKeyword = false;
-    for (var i=0, length = dependencies.length; i<length; i++) {
-      var p = dependencies[i];
-      if (dependencyWillFind.isEqualTo(p)) {
-        arrayContainsDependencyWithKeyword = true;
-      }
-      if (dependencyWontFind.isEqualTo(p)) {
-        arrayContainsDependencyWithoutKeyword = true;
-      }
-    }
-    (arrayContainsDependencyWithKeyword).should.be.equal(true);
-    (arrayContainsDependencyWithoutKeyword).should.be.equal(false);
+    var dependencyWillFind = module2,
+        dependencyWontFind = module3;
+
+    (isDependencyInTree(dependencyWillFind, dependencies)).should.be.equal(true);
+    (isDependencyInTree(dependencyWontFind, dependencies)).should.be.equal(false);
+  });
+
+  it('should only retrieve production dependencies', function () {
+    var dependencyWillFind = module1,
+        dependencyWontFind = module2,
+        productionDependencies = dependenciesInfo({ path: searchRootPath, type: ['dependencies'] });
+
+    (isDependencyInTree(dependencyWillFind, productionDependencies)).should.be.equal(true);
+    (isDependencyInTree(dependencyWontFind, productionDependencies)).should.be.equal(false);
   });
 
 });
