01fba0c7d91f8f7e1924285f425778471c22e5c3
David Marchena <dvmarchena@gmail.com>
2015-05-17 18:20:51 +0200

    Return the tree object instead of an array of dependencies

--- test/scenarios/npm/test.js
+++ test/scenarios/npm/test.js
@@ -6,69 +6,64 @@ var path = require('path'),
     scenarioDir = __dirname,
     testDataDir = path.join(scenarioDir, '/data/'),
     Dependency = require(path.join(rootDir, '/lib/dependency.js')),
-    dependenciesInfo = require(path.join(rootDir, '/lib/'));
-
-function isDependencyInTree(dependencyObj, tree) {
-  for (var i=0, length = tree.length; i<length; i++) {
-    var p = tree[i];
-    if (dependencyObj.isEqualTo(p)) {
-      return true;
-    }
-  }
-  return false;
-}
+    dependencyInfo = require(path.join(rootDir, '/lib/'));
     
-describe('NPM Dependency search:', function () {
+describe('DependencyTree', function () {
 
   var module1 = new Dependency('module-1', path.join(testDataDir, '/node_modules/module-1/')),
       module2 = new Dependency('module-2', path.join(testDataDir, '/node_modules/module-2/')),
       module3 = new Dependency('module-3', path.join(testDataDir, '/node_modules/module-3/')),
       searchRootPath = testDataDir,
-      dependencies = dependenciesInfo({ path: searchRootPath });
+      tree = dependencyInfo.createTree({ 
+        path: searchRootPath 
+      }),
+      filteredTree = dependencyInfo.createTree({ 
+        path: searchRootPath,
+        filterBy: {
+          keyword: 'search-target'
+        }
+      }),
+      prodTree = dependencyInfo.createTree({ 
+        path: searchRootPath, 
+        type: ['dependencies'] 
+      }),
+      devTree = dependencyInfo.createTree({ 
+        path: searchRootPath, 
+        type: ['devDependencies'] 
+      });
 
-  it('should return an array', function () {
-    (dependencies).should.be.an.instanceOf(Array);
+  it('should be an array', function () {
+    (tree.dependencies).should.be.an.instanceOf(Array);
   });
 
-  if (dependencies.length>0) {
+  if (tree.dependencies.length>0) {
     it('should return an array of Dependency objects', function () {
-      (dependencies[0]).should.be.an.instanceOf(Dependency);
+      (tree.dependencies[0]).should.be.an.instanceOf(Dependency);
     });
   }
 
   it('should filter dependencies by its keywords', function () {
     var dependencyWillFind = module2,
-        dependencyWontFind = module3,
-        filteredDependencies = dependenciesInfo({ 
-          path: searchRootPath,
-          filterBy: {
-            keyword: 'search-target'
-          }
-        });
+        dependencyWontFind = module3;
 
-    (isDependencyInTree(dependencyWillFind, filteredDependencies)).should.be.equal(true);
-    (isDependencyInTree(dependencyWontFind, filteredDependencies)).should.be.equal(false);
+    (filteredTree.contains(dependencyWillFind)).should.be.equal(true);
+    (filteredTree.contains(dependencyWontFind)).should.be.equal(false);
   });
 
   it('should only retrieve production dependencies', function () {
     var dependencyWillFind = module1,
-        dependencyWontFind = module2,
-        productionDependencies = dependenciesInfo({ 
-          path: searchRootPath, 
-          type: ['dependencies'] 
-        });
+        dependencyWontFind = module2;
 
-    (isDependencyInTree(dependencyWillFind, productionDependencies)).should.be.equal(true);
-    (isDependencyInTree(dependencyWontFind, productionDependencies)).should.be.equal(false);
+    (prodTree.contains(dependencyWillFind)).should.be.equal(true);
+    (prodTree.contains(dependencyWontFind)).should.be.equal(false);
   });
 
   it('should only retrieve dev dependencies', function () {
     var dependencyWillFind = module2,
-        dependencyWontFind = module1,
-        productionDependencies = dependenciesInfo({ path: searchRootPath, type: ['devDependencies'] });
+        dependencyWontFind = module1;
 
-    (isDependencyInTree(dependencyWillFind, productionDependencies)).should.be.equal(true);
-    (isDependencyInTree(dependencyWontFind, productionDependencies)).should.be.equal(false);
+    (devTree.contains(dependencyWillFind)).should.be.equal(true);
+    (devTree.contains(dependencyWontFind)).should.be.equal(false);
   });
 
 });
